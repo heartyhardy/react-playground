6bb57239eeb831c495210668876a90090726e3e4
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2019-03-20 14:54:22 +0530

    Added function param binding

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     ]
   }
 
-  swapName_handler = () => {
+  swapName_handler = (newName) => {
     let orphand1 = 0, orphand2 = 0
 
     while(orphand1 === orphand2)
@@ -28,14 +28,24 @@ class App extends Component {
     this.setState({persons});
   }
 
+  setNewName = (newName) => {
+    this.setState({
+      persons: [
+        { name: newName, age: 30 },
+        { name: "Mel", age: 20 },
+        { name: "Moi", age: 50 }
+      ]
+    })
+  }
+
   render() {
     const { persons } = this.state;
     return (
       <div className="App">
         <button onClick={this.swapName_handler}>Random Swap</button>
-        <Person name={persons[0].name} age={persons[0].age} />
+        <Person name={persons[0].name} age={persons[0].age} clickHandler={this.swapName_handler}/>
         <Person name={persons[1].name} age={persons[1].age}>Hobbies: Sky diving</Person>
-        <Person name={persons[2].name} age={persons[2].age} />
+        <Person name={persons[2].name} age={persons[2].age} clickHandler={this.setNewName.bind(this,'Maya')} />
       </div>
     );
   }
--- src/components/Person.js
+++ src/components/Person.js
@@ -7,7 +7,9 @@ const person = (props) => {
     return (
         <div>
             <h1>I'm {props.name} {props.age}</h1>
+            <hr hidden={props.hr_visible} />
             <p>{props.children}</p>
+            <p onClick={props.clickHandler} hidden={(props.clickHandler)?false:true} >Change name</p>
         </div>
     )
 }
